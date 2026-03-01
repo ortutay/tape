dc3b243ba623e95a962eb7e576077abb948a1907
ortutay <marcell.ortutay@gmail.com>
2026-03-01 12:44:10 -0800

    refactor

--- tape3.js
+++ tape3.js
@@ -3,39 +3,46 @@ import * as fox from 'fetchfox-sdk';
 const api_key = process.env.FETCHFOX_API_KEY;
 fox.configure({
   apiKey: api_key,
-  host: 'https://api.fetchfox.ai',
+  host: 'https://staging.api.fetchfox.ai',
+  // host: 'http://localhost:3030',
 });
 
 const run = async () => {
+  const maxVisits = 10;
   const top = 'https://shop.eriks.nl/en/maintenance-products/maintenance-products-tapes-and-accessories/maintenance-products-tapes-and-accessories-tapes/';
 
-  const out1 = await fox.agent({
-    prompt: 'Crawl: Find the dozen or so tape category URLs on this page maxVisits=1 ' + top,
-  });
-  const categories = out1.results.hits;
-  console.log('categories:', categories);
-
-  const out2 = await fox.agent({
-    prompt: 'Find all product detail URLs linked from these categories  maxVisits=10 ' + categories.join(' '),
-  });
-  const products = out2.results.hits;
-  console.log('products:', products);
-
-  const out3 = await fox.agent({
-    prompt: 'Check each of these pages for links to additional product detail URLs maxVisits=10 ' + products.join(' '),
-  });
-  const products2 = out3.results.hits;
-  console.log('products2:', products2);
-
-  const allProducts = [
-    ...products,
-    ...products2,
+  const steps = [
+    'Find the tape category URLs',
+    'Find product detail URLs',
+    'Find URLs of product variants. Return empty results if no product variants found on this page.',
   ];
 
-  const out4 = await fox.agent({
-    prompt: 'Extract product name, price in EUR, and dimensions as length in meters and width in mm maxExtracts=10 ' + allProducts.join(' '),
-  });
-  console.log('items:', out4.results.items);
+  let urls = [top];
+  const hits = [];
+  for (const step of steps) {
+    const out = await fox.crawl({
+      query: step,
+      startUrls: urls,
+      maxVisits,
+      maxDepth: 0,
+    });
+    urls = out.results.hits;
+    console.log('Step:', step);
+    console.log('Hits:', urls);
+    hits.push(urls);
+  }
+
+  const productUrls = hits[1];
+  const variantUrls = hits[2];
+  const delta = new Set(variantUrls)
+    .difference(new Set(productUrls))
+    .size;
+  console.log('New URLs from variants:', delta);
+
+  const allProductUrls = [...(new Set([...productUrls, ...variantUrls]))].sort();
+
+  console.log('All product URLs:', allProductUrls);
+  console.log(allProductUrls.length);
 }
 
 run();
